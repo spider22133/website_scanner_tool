49215e8bf52365baeec7ddc45f1e53db4ff8adf0
Евгений Слюсаренко <e.slyusarenko@hotmail.com>
2021-07-22 09:29:21 +0200

    ListItem active on click

--- config_module/src/components/webseites-list-item.component.tsx
+++ config_module/src/components/webseites-list-item.component.tsx
@@ -1,13 +1,19 @@
 import IWebsite from '../interfaces/website.interface';
 
 type ItemProps = {
-    key: number;
+    index: number;
     website: IWebsite;
+    currentIndex: number;
+    setActiveTutorial: SetActiveTutorial;
 }
-export default function WebsitesListItem({website, key}: ItemProps) {
+
+type SetActiveTutorial = (website: IWebsite, index: number) => void;
+
+export default function WebsitesListItem({ website, index, currentIndex, setActiveTutorial }: ItemProps) {
     return (
         <>
-            <li key={key} className="list-group-item active">{website.name}</li>
+            <li className={`list-group-item ${index === currentIndex ? "active" : ""}`}
+                onClick={() => setActiveTutorial(website, index)}>{website.name}</li>
         </>
     );
 }
--- config_module/src/components/websites-list.tsx
+++ config_module/src/components/websites-list.tsx
@@ -1,34 +1,43 @@
 import WebsitesListItem from "./webseites-list-item.component";
 import WebsiteDataService from "../services/website.service";
 import { useEffect, useState } from "react";
-import IWebsite from '../interfaces/website.interface'
+import IWebsite from '../interfaces/website.interface';
 
 export default function WebsitesList() {
     const [websites, setWebsites] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(-1);
 
     useEffect(() => {
         getWebsites();
-      }, []);
+    }, []);
 
     const getWebsites = () => {
         WebsiteDataService.getAll()
-        .then(response => {
-            setWebsites(response.data.data)
-            console.log(response.data.data);
-        })
+            .then(response => {
+                setWebsites(response.data.data)
+            })
     }
+
+    const setActiveTutorial = (website: IWebsite, index: number) => {
+        setCurrentIndex(index);
+    }
+
     return (
-        <>
+        <div className="my-4">
             <div className="col-4">
                 <ul className="list-group">
                     {websites && websites.map((website: IWebsite, index) => (
-                        <WebsitesListItem key={index} website={website}/>
+                        <WebsitesListItem setActiveTutorial={setActiveTutorial}
+                            key={index}
+                            index={index}
+                            currentIndex={currentIndex}
+                            website={website} />
                     ))}
                 </ul>
             </div>
             <div className="col-8">
 
             </div>
-        </>
+        </div>
     );
 }
