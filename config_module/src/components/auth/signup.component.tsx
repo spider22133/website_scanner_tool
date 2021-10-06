75deccf612122d551829628970ed67034ff96827
Евгений Слюсаренко <e.slyusarenko@hotmail.com>
2021-10-06 10:32:03 +0200

    Login page height fix

--- config_module/src/components/auth/signup.component.tsx
+++ config_module/src/components/auth/signup.component.tsx
@@ -30,7 +30,7 @@ export default function SignUp() {
   };
 
   return (
-    <section className="vh-100" style={{ backgroundColor: '#508bfc' }}>
+    <section className="vh-100-c" style={{ backgroundColor: '#508bfc' }}>
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-12 col-md-8 col-lg-6 col-xl-4">
