2f82527d869b4a923f216d4df4a23913c3ae489e
spider22133 <evgeniy.slyusarenko@gmail.com>
2025-02-21 14:31:50 +0100

    Refactoring

--- scanner_module/src/controllers/websites.controller.ts
+++ scanner_module/src/controllers/websites.controller.ts
@@ -1,123 +1,123 @@
-import { NextFunction, Request, Response } from 'express';
-import { Website } from '@/interfaces/website.interface';
-import WebsiteService from '@services/websites.service';
-import CreateWebsiteDto from '@dtos/website.dto';
-import SoftwareVersionChecker from '@/softwareVersionChecker';
-import WebsiteStatesService from '@services/website_states.service';
-import { WebsiteModel } from '@models/website.model';
-import WebsiteControlStepsService from '@services/website_control_steps.service';
+import { NextFunction, Request, Response } from 'express'
+import { Website } from '@/interfaces/website.interface'
+import WebsiteService from '@services/websites.service'
+import CreateWebsiteDto from '@dtos/website.dto'
+import SoftwareVersionChecker from '@/softwareVersionChecker'
+import WebsiteStatesService from '@services/website_states.service'
+import { WebsiteModel } from '@models/website.model'
+import WebsiteControlStepsService from '@services/website_control_steps.service'
 
 class WebsitesController {
-  public websiteService = new WebsiteService();
-  public softwareVersionChecker;
-  public websiteStatesService = new WebsiteStatesService();
-  public websiteControlStepsService = new WebsiteControlStepsService();
+  public websiteService = new WebsiteService()
+  public softwareVersionChecker
+  public websiteStatesService = new WebsiteStatesService()
+  public websiteControlStepsService = new WebsiteControlStepsService()
 
   constructor(softwareVersionChecker: SoftwareVersionChecker) {
-    this.softwareVersionChecker = softwareVersionChecker;
+    this.softwareVersionChecker = softwareVersionChecker
   }
 
   public getWebsites = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const findAllWebsitesData: Website[] = await this.websiteService.findAllWebsites();
+      const findAllWebsitesData: Website[] = await this.websiteService.findAllWebsites()
 
-      res.status(200).json({ data: findAllWebsitesData, message: 'findAll' });
+      res.status(200).json({ data: findAllWebsitesData, message: 'findAll' })
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 
   public getWebsiteById = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const websiteId = Number(req.params.id);
-      const findOne: Website = await this.websiteService.findWebsiteById(websiteId);
+      const websiteId = Number(req.params.id)
+      const findOne: Website = await this.websiteService.findWebsiteById(websiteId)
 
-      res.status(200).json({ data: findOne, message: 'findOne' });
+      res.status(200).json({ data: findOne, message: 'findOne' })
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 
   public getWebsiteMainStepStates = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const websiteId = Number(req.params.id);
-      const steps = await this.websiteControlStepsService.findControlStepsByWebsiteId(websiteId);
-      const mainStep = steps.find(step => step.type === 'MAIN');
-      const states = await this.websiteStatesService.findStatesByStepId(mainStep.id);
+      const websiteId = Number(req.params.id)
+      const steps = await this.websiteControlStepsService.findControlStepsByWebsiteId(websiteId)
+      const mainStep = steps.find(step => step.type === 'MAIN')
+      const states = await this.websiteStatesService.findStatesByStepId(mainStep.id)
 
-      res.status(200).json({ data: states, message: 'findOne' });
+      res.status(200).json({ data: states, message: 'findOne' })
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 
   public updateWebsite = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const websiteId = Number(req.params.id);
-      const websiteData: CreateWebsiteDto = req.body;
-      const { status, msg } = await this.softwareVersionChecker.checkUrl(websiteData.url);
+      const websiteId = Number(req.params.id)
+      const websiteData: CreateWebsiteDto = req.body
+      const { status, msg } = await this.softwareVersionChecker.checkUrl(websiteData.url)
 
-      if (status !== 200) next(msg);
+      if (status !== 200) next(msg)
 
-      const updateWebsiteData = await this.websiteService.updateWebsite(websiteId, { ...websiteData, is_active: true });
-      res.status(200).json({ data: updateWebsiteData, message: 'updated' });
+      const updateWebsiteData = await this.websiteService.updateWebsite(websiteId, { ...websiteData, is_active: true })
+      res.status(200).json({ data: updateWebsiteData, message: 'updated' })
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 
   public checkWebsite = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const websiteId = Number(req.params.id);
-      const findOne: WebsiteModel = await this.websiteService.findWebsiteById(websiteId);
+      const websiteId = Number(req.params.id)
+      const findOne: WebsiteModel = await this.websiteService.findWebsiteById(websiteId)
 
-      await this.softwareVersionChecker.checkWebsite(findOne);
+      await this.softwareVersionChecker.checkWebsite(findOne)
 
-      const steps = await this.websiteControlStepsService.findControlStepsByWebsiteId(websiteId);
-      const mainStep = steps.find(step => step.type === 'MAIN');
-      const latestState = await this.websiteStatesService.findLatestStateByStepId(mainStep.id);
+      const steps = await this.websiteControlStepsService.findControlStepsByWebsiteId(websiteId)
+      const mainStep = steps.find(step => step.type === 'MAIN')
+      const latestState = await this.websiteStatesService.findLatestStateByStepId(mainStep.id)
 
-      res.status(200).json({ data: latestState, message: 'checked' });
+      res.status(200).json({ data: latestState, message: 'checked' })
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 
   public createWebsite = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const websiteData: CreateWebsiteDto = req.body;
-      const { status, msg } = await this.softwareVersionChecker.checkUrl(websiteData.url);
+      const websiteData: CreateWebsiteDto = req.body
+      const { status, msg } = await this.softwareVersionChecker.checkUrl(websiteData.url)
 
-      if (status !== 200) next(msg);
+      if (status !== 200) next(msg)
 
-      const createWebsiteData = await this.websiteService.createWebsite({ ...websiteData, is_active: true });
-      res.status(201).json({ data: createWebsiteData, message: 'created' });
+      const createWebsiteData = await this.websiteService.createWebsite({ ...websiteData, is_active: true })
+      res.status(201).json({ data: createWebsiteData, message: 'created' })
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 
   public deleteWebsite = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const websiteId = Number(req.params.id);
-      const deleteWebsiteData: Website = await this.websiteService.deleteWebsite(websiteId);
+      const websiteId = Number(req.params.id)
+      const deleteWebsiteData: Website = await this.websiteService.deleteWebsite(websiteId)
 
-      res.status(200).json({ data: deleteWebsiteData, message: 'deleted' });
+      res.status(200).json({ data: deleteWebsiteData, message: 'deleted' })
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 
   public searchWebsite = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const searchString = String(req.params.query);
-      const searchWebsiteData: Website[] = await this.websiteService.searchQuery(searchString);
+      const searchString = String(req.params.query)
+      const searchWebsiteData: Website[] = await this.websiteService.searchQuery(searchString)
 
-      res.status(200).json({ data: searchWebsiteData });
+      res.status(200).json({ data: searchWebsiteData })
     } catch (error) {
-      next(error);
+      next(error)
     }
-  };
+  }
 }
 
-export default WebsitesController;
+export default WebsitesController
